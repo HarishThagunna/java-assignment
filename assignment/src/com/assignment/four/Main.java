23b6120f375aee9237ddef797c43cdf7e49ffe1a
thagunna.harish8@gmail.com <hthagunna@deerwalk.com>
2020-05-17 20:36:47 +0545

    assignment 4

--- assignment/src/com/assignment/four/Main.java
+++ assignment/src/com/assignment/four/Main.java
@@ -1,8 +1,5 @@
 package com.assignment.four;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
     public static void main(String a[]) {
         BinaryTreeOperations bst = new BinaryTreeOperations();
@@ -12,61 +9,99 @@ public class Main {
 
         int [] numsForBinaryTree = {};
 
-       /* HashMap<String, Integer> operations = new HashMap<String, Integer>();
-        for(int loop=0;loop<=0;loop++){
-            numsForBinaryTree = inputStorage.getNumberSets(0).numbers;
-            operations = inputStorage.getNumberSets(0).operations;
+        for(int loop=0;loop<7;loop++){
+            System.out.println("--------Start Of Set "+(loop+1)+" Operations------");
+            numsForBinaryTree = inputStorage.getNumberSets(loop);
 
-            for (Map.Entry<String,Integer> entry : operations.entrySet()){
-                System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+            /* 1. Create a binary tree*/
+            for(int i=0;i<numsForBinaryTree.length;i++){
+                if(numsForBinaryTree[i] != -999){
+                    bst.insert(numsForBinaryTree[i]);
+                }
+            }
+            System.out.println("\nTree Creation Completed...");
+
+            /* 2. Print the tree using "inorder", 'preorder', and 'postorder */
+            System.out.println("\nIn-Order : ");
+            bst.inOrderTraversal();
+            System.out.println("\nPre-Order : ");
+            bst.preOrderTraversal();
+            System.out.println("\nPost-Order : ");
+            bst.postOrderTraversal();
+
+            /* 3. Call a súbroutine "count", which returns the number of nodes in the tree */
+            System.out.println("\n\nNumber of Nodes in a Tree: "+ bst.countNodes());
+
+            /* 4. Call a subroutine "children" which prints the number of children each node has */
+
+            /* 5. Insert and delete several nodes according to the Instructions given */
+
+            switch (loop){
+                case 0:
+                    System.out.println("\nSet 1 Operations");
+                    bst.insert(21);
+                    bst.delete(1);
+                    bst.insert(0);
+                    bst.delete(10);
+                    bst.delete(11);
+                    bst.delete(5);
+                    bst.delete(2);
+                    bst.insert(10);
+                    break;
+                case 1:
+                    System.out.println("\nSet 2 Operations");
+                    bst.delete(3);
+                    bst.delete(1);
+                    break;
+                case 2:
+                    System.out.println("\nSet 3 Operations");
+                    bst.delete(15);
+                    bst.delete(20);
+                    bst.insert(30);
+                    bst.delete(10);
+                    bst.insert(5);
+                    bst.insert(10);
+                    bst.delete(5);
+                    bst.delete(15);
+                    bst.insert(20);
+                    bst.delete(30);
+                    break;
+                case 3:
+                    System.out.println("\nSet 4 Operations");
+                    bst.delete(2);
+                case 4:
+                    System.out.println("\nSet 5 Operations");
+                    bst.delete(37);
+                    bst.delete(15);
+                    bst.insert(40);
+                    bst.insert(99);
+                    break;
+                case 5:
+                    System.out.println("\nSet 6 Operations");
+                    break;
+                case 6:
+                    System.out.println("\nSet 7 Operations");
+                    break;
+                default:
+                    break;
             }
 
-        }*/
+            /* 6. Print the tree again using 'inorder, "preorder", and "postorder */
+            System.out.println("In-Order Current: ");
+            bst.inOrderTraversal();
+            System.out.println("\nPre-Order Current: ");
+            bst.preOrderTraversal();
+            System.out.println("\nPost-Order Current: ");
+            bst.postOrderTraversal();
 
-        numsForBinaryTree = new int []{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,-999};
-        /* 1. Create a binary tree*/
-        for(int i=0;i<numsForBinaryTree.length;i++){
-            if(numsForBinaryTree[i] != -999){
-                bst.insert(numsForBinaryTree[i]);
-            }
+            /* 7. Call subroutine *count again, which returns the number of nodes in the tree */
+            System.out.println("\n\nCurrent Number of Nodes in a Tree: "+ bst.countNodes());
+
+            /* 8. Call a subroutine *children" again, which prints the number of children each node has */
+            /* 9. Free the tree */
+            bst.free();
+
+            System.out.println("--------End of Set "+(loop+1)+" Operations------\n");
         }
-
-        /* 2. Print the tree using "inorder", 'preorder', and 'postorder */
-        System.out.println("\nIn-Order : ");
-        bst.inOrderTraversal();
-        System.out.println("\nPre-Order : ");
-        bst.preOrderTraversal();
-        System.out.println("\nPost-Order : ");
-        bst.postOrderTraversal();
-
-        /* 3. Call a súbroutine "count", which returns the number of nodes in the tree */
-        System.out.println("\n\nNumber of Nodes in a Tree: "+ bst.countNodes());
-
-        /* 4. Call a subroutine "children" which prints the number of children each node has */
-        /* 5. Insert and delete several nodes according to the Instructions given */
-
-        bst.insert(26);
-        bst.delete(17);
-        bst.delete(10);
-
-        /* 6. Print the tree again using 'inorder, "preorder", and "postorder */
-        System.out.println("In-Order Current: ");
-        bst.inOrderTraversal();
-        System.out.println("\nPre-Order Current: ");
-        bst.preOrderTraversal();
-        System.out.println("\nPost-Order Current: ");
-        bst.postOrderTraversal();
-
-        /* 7. Call subroutine *count again, which returns the number of nodes in the tree */
-        System.out.println("\n\nCurrent Number of Nodes in a Tree: "+ bst.countNodes());
-
-        /* 8. Call a subroutine *children" again, which prints the number of children each node has */
-        /* 9. Free the tree */
-        bst.free();
-
-        /*System.out.println("Final InOrder");
-        bst.inOrderTraversal();
-        bst.insert(5);
-        bst.inOrderTraversal();*/
     }
 }
